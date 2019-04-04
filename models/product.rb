02838bf4610b27415ed5c346a8ac5c1ea2950443
daniel sc <su7cheng@gmail.com>
2019-04-04 16:32:57 -0300

    Create sample_locations method.

--- models/product.rb
+++ models/product.rb
@@ -5,26 +5,39 @@ require 'json'
 
 class Product
 
-   # ping the API for the product JSON
-   url = 'https://fomotograph-api.udacity.com/products.json'
-   DATA = HTTParty.get(url)['photos']
+  # read access for the Product attributes
+  attr_reader :id, :title, :location, :summary, :url, :price
 
-   # initialize a Product object using a data hash
-   def initialize(product_data = {})
-     @id = product_data['id']
-     @title = product_data['title']
-     @location = product_data['location']
-     @summary = product_data['summary']
-     @url = product_data['url']
-     @price = product_data['price']
-   end
+  # locations offered by Fomotograph
+  LOCATIONS = ['canada', 'england', 'france', 'ireland', 'mexico', 'scotland', 'taiwan', 'us']
 
-   # return an array of Product objectsc
-   def self.all
-     DATA.map { |product| new(product) }
-   end
+  # ping the API for the product JSON
+  url = 'https://fomotograph-api.udacity.com/products.json'
+  DATA = HTTParty.get(url)['photos']
 
+  # initialize a Product object using a data hash
+  def initialize(product_data = {})
+    @id = product_data['id']
+    @title = product_data['title']
+    @location = product_data['location']
+    @summary = product_data['summary']
+    @url = product_data['url']
+    @price = product_data['price']
+  end
 
+  # return an array of Product objectsc
+  def self.all
+    DATA.map { |product| new(product) }
+  end
 
- end
+  # return an array of sample products from each location
+  def self.sample_locations
+    @products = []
+    LOCATIONS.each do |location|
+      @products.push self.all.select { |product| product.location == location }.sample
+    end
+    return @products
+  end 
+
+end
 
