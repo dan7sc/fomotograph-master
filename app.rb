2b7a249c3f203a6befd24de3263e23bf84b47043
daniel sc <su7cheng@gmail.com>
2019-04-04 18:11:56 -0300

    Add single.erb view.

--- app.rb
+++ app.rb
@@ -29,41 +29,7 @@ end
 
 get '/products/:id' do
   # PAGE DISPLAYING ONE PRODUCT WITH A GIVEN ID
-  DATA = HTTParty.get('https://fomotograph-api.udacity.com/products.json')['photos']
-  @product = DATA.select { |prod| prod['id'] == params[:id].to_i }.first
-  erb "<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Fomotograph | <%= @product['title'] %> </title>
-    <link rel='stylesheet' type='text/css' href='<%= url('/style.css') %>'>
-    <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,500,600' rel='stylesheet' type='text/css'>
-  </head>
-
-  <body>
-
-    <div id='container'>
-
-      <div id='header'>
-        <a href='/'><img src='/logo-black-text.png' alt='logo image' class='logo'/></a>
-        <a href='/team' class='nav'>Team</a>
-        <a href='/products' class='nav'>Products</a>
-      </div>
-
-      <div id='main'>
-        <h1><%= @product['title'] %></h1>
-        <a class='small-button' href='#'>Fomotograph Me!</a>
-        <p class='summary'> <%= @product['summary'] %> </p>
-        <p class='summary'>Order your prints today for $<%= @product['price'] %></p>
-        <img class='full' src='<%= @product['url'] %>' />
-        <a class='small-button' href='/products/location/<%= @product['location'] %>'> View All <%= @product['location'] != 'us' ? @product['location'].capitalize : @product['location'].upcase %> Products </a>
-      </div>
-
-      <div id='footer'>
-        © Fomotograph
-      </div>
-
-    </div>
-
-  </body>
-  </html>"
+  @product = Product.find(params[:id])
+  @page_title = @product.title 
+  erb :single
 end
